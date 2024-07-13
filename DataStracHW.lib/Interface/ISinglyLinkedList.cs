074d2c65c8324d54ebd7aac4e300b0173465e247
Zoriana Zhuk <zoriana.zh08@gmail.com>
2024-07-13 15:45:01 +0300

    Додано інтерфейси

---
+++ DataStracHW.lib/Interface/ISinglyLinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStracHW.lib.Interface
+{
+    public interface ISinglyLinkedList : ICollection
+    {
+        void Add(object data);
+    }
+}
---
+++ DataStracHW.lib/Interface/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStracHW.lib.Interface
+{
+    public interface IStack : ICollection
+    {
+        void Push(object item);
+        object Pop();
+        object Peek();
+    }
+}
+
