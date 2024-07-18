6ada6d737881239117fc2a1db807ff90d2502ff7
Zoriana Zhuk <zoriana.zh08@gmail.com>
2024-07-18 11:55:32 +0300

    Generics HomeWork

--- DataStracHW.lib/Stack.cs
+++ DataStracHW.lib/Stack.cs
@@ -1,4 +1,5 @@
-﻿using DataStracHW.lib.Interface;
+﻿using DataStracHW.lib.Generic;
+using DataStracHW.lib.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace DataStracHW.lib
 {
-    public class Stack : IStack
+    public class Stack<T> : IStack<T>
     {
-        private object[] _items;
+        private T[] _items;
         private int _count;
 
         public Stack(int capacity = 4)
         {
-            _items = new object[capacity];
+            _items = new T[capacity];
             _count = 0;
         }
 
@@ -23,18 +24,19 @@ namespace DataStracHW.lib
             get { return _count; }
         }
 
-        public void Push(object item)
+        public void Push(T item)
         {
             EnsureCapacity(_count + 1);
             _items[_count] = item;
             _count++;
         }
+
         private void EnsureCapacity(int capacity)
         {
             if (_items.Length < capacity)
             {
                 int newCapacity = _items.Length == 0 ? 4 : _items.Length * 2;
-                object[] newItems = new object[newCapacity];
+                T[] newItems = new T[newCapacity];
                 for (int i = 0; i < _count; i++)
                 {
                     newItems[i] = _items[i];
@@ -42,32 +44,32 @@ namespace DataStracHW.lib
                 _items = newItems;
             }
         }
-        public object Peek()
+
+        public T Peek()
         {
             if (_count == 0)
             {
-                Console.WriteLine("Стек порожній.");
-                return null;
+                throw new InvalidOperationException("Стек порожній.");
             }
             return _items[_count - 1];
         }
-        public object Pop()
+
+        public T Pop()
         {
             if (_count == 0)
             {
-                Console.WriteLine("Стек порожній.");
-                return null;
+                throw new InvalidOperationException("Стек порожній.");
             }
 
-            object item = _items[_count - 1];
-            _items[_count - 1] = null;
+            T item = _items[_count - 1];
+            _items[_count - 1] = default(T);
             _count--;
             return item;
         }
 
-        public object[] ToArray()
+        public T[] ToArray()
         {
-            object[] newArray = new object[_count];
+            T[] newArray = new T[_count];
             for (int i = 0; i < _count; i++)
             {
                 newArray[i] = _items[i];
@@ -79,12 +81,12 @@ namespace DataStracHW.lib
         {
             for (int i = 0; i < _items.Length; i++)
             {
-                _items[i] = null;
+                _items[i] = default(T);
             }
             _count = 0;
         }
 
-        public bool Contains(object item)
+        public bool Contains(T item)
         {
             for (int i = 0; i < _count; i++)
             {
