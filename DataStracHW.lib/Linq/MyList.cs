c9bf899c81183dcc47ab7820fad7047957cfaf46
Zoriana Zhuk <zoriana.zh08@gmail.com>
2024-08-07 09:07:02 +0300

    HomeWork#9

---
+++ DataStracHW.lib/Linq/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStracHW.lib.Linq
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
